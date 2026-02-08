76c9edb95807f9461075e11a73c4df90b6f1ff4e
kotobazza <152398719+kotobazza@users.noreply.github.com>
2026-02-08 17:21:30 +0300

    refactor: construct default application and package for project

---
+++ src/main/java/com/kotobazza/kotoblog/KotoblogApplication.java
@@ -0,0 +1,12 @@
+package com.kotobazza.kotoblog;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KotoblogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(KotoblogApplication.class, args);
+    }
+}
